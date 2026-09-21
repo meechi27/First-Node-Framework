fdf72d91b0d965b96d4835dc00cf83ff2274c1b3
meechi27 <daawaboy27@outlook.com>
2026-09-21 14:28:53 +0100

    Setup for my first request response

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,14 @@ function matchRoutes(){
 }
 
 const server = http.createServer((req,res)=>{
-
+    console.log(req.url);
+    console.log(req.method);
+    
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/plain");
+    res.write("Hello user!");
+    res.end("Close.");
+    
 });
+
+server.listen(3000,()=>console.log("The server is Live on port 3000!"));
