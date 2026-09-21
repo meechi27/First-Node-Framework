f9243d278566c76ac801c780d7b04692bdb19f3a
meechi27 <daawaboy27@outlook.com>
2026-09-21 18:59:13 +0100

    temporarely added guards for those two routes
    when / route was called it automatically called these two favicon & sw.js routes

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,21 @@ const server = http.createServer((req,res)=>{
     }
     // we create a new object that splits url into path and params and handles edge cases of parsing
     const url = new URL(req.url , "http://localhost");
+
+
+    // temporary guard so we don't see those request appear in console 
+    if(url.pathname === "/favicon.ico" || url.pathname === "/sw.js"){
+        res.statusCode = 404;
+        res.end("not found!");
+        return;
+    }
+
+
     console.log(req.method);
     console.log(url.pathname);
     console.log(url.searchParams);
+
+    res.end("Under construction !");
     
 });
 
